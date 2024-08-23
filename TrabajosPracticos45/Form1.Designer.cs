6465826f55f57bb0f5bee04a147f7c7fb6e58515
malenpinto <malenpinto9@gmail.com>
2024-08-23 00:21:15 -0300

    Generar Funcion ok

--- TrabajosPracticos45/Form1.Designer.cs
+++ TrabajosPracticos45/Form1.Designer.cs
@@ -28,13 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TDesde = new System.Windows.Forms.TextBox();
+            this.THasta = new System.Windows.Forms.TextBox();
+            this.BGenerarFuncion = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BNumPares = new System.Windows.Forms.Button();
+            this.BNumPrimos = new System.Windows.Forms.Button();
+            this.BNumImpares = new System.Windows.Forms.Button();
+            this.TBListaNumeros = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Desde";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(53, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hasta";
+            // 
+            // TDesde
+            // 
+            this.TDesde.Location = new System.Drawing.Point(133, 101);
+            this.TDesde.Name = "TDesde";
+            this.TDesde.Size = new System.Drawing.Size(94, 22);
+            this.TDesde.TabIndex = 2;
+            this.TDesde.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TDesde_KeyPress);
+            // 
+            // THasta
+            // 
+            this.THasta.Location = new System.Drawing.Point(133, 154);
+            this.THasta.Name = "THasta";
+            this.THasta.Size = new System.Drawing.Size(94, 22);
+            this.THasta.TabIndex = 3;
+            this.THasta.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.THasta_KeyPress);
+            // 
+            // BGenerarFuncion
+            // 
+            this.BGenerarFuncion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BGenerarFuncion.Location = new System.Drawing.Point(35, 222);
+            this.BGenerarFuncion.Name = "BGenerarFuncion";
+            this.BGenerarFuncion.Size = new System.Drawing.Size(208, 28);
+            this.BGenerarFuncion.TabIndex = 4;
+            this.BGenerarFuncion.Text = "Generar Función";
+            this.BGenerarFuncion.UseVisualStyleBackColor = true;
+            this.BGenerarFuncion.Click += new System.EventHandler(this.BGenerarFuncion_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(377, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Lista de Números";
+            // 
+            // BNumPares
+            // 
+            this.BNumPares.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BNumPares.Location = new System.Drawing.Point(35, 270);
+            this.BNumPares.Name = "BNumPares";
+            this.BNumPares.Size = new System.Drawing.Size(208, 28);
+            this.BNumPares.TabIndex = 7;
+            this.BNumPares.Text = "Números Pares";
+            this.BNumPares.UseVisualStyleBackColor = true;
+            // 
+            // BNumPrimos
+            // 
+            this.BNumPrimos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BNumPrimos.Location = new System.Drawing.Point(35, 367);
+            this.BNumPrimos.Name = "BNumPrimos";
+            this.BNumPrimos.Size = new System.Drawing.Size(208, 28);
+            this.BNumPrimos.TabIndex = 8;
+            this.BNumPrimos.Text = "Números Primos";
+            this.BNumPrimos.UseVisualStyleBackColor = true;
+            // 
+            // BNumImpares
+            // 
+            this.BNumImpares.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BNumImpares.Location = new System.Drawing.Point(35, 319);
+            this.BNumImpares.Name = "BNumImpares";
+            this.BNumImpares.Size = new System.Drawing.Size(208, 28);
+            this.BNumImpares.TabIndex = 9;
+            this.BNumImpares.Text = "Números Impares";
+            this.BNumImpares.UseVisualStyleBackColor = true;
+            // 
+            // TBListaNumeros
+            // 
+            this.TBListaNumeros.FormattingEnabled = true;
+            this.TBListaNumeros.ItemHeight = 16;
+            this.TBListaNumeros.Location = new System.Drawing.Point(281, 103);
+            this.TBListaNumeros.Name = "TBListaNumeros";
+            this.TBListaNumeros.Size = new System.Drawing.Size(363, 340);
+            this.TBListaNumeros.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(693, 501);
+            this.Controls.Add(this.TBListaNumeros);
+            this.Controls.Add(this.BNumImpares);
+            this.Controls.Add(this.BNumPrimos);
+            this.Controls.Add(this.BNumPares);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BGenerarFuncion);
+            this.Controls.Add(this.THasta);
+            this.Controls.Add(this.TDesde);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Formulario4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TDesde;
+        private System.Windows.Forms.TextBox THasta;
+        private System.Windows.Forms.Button BGenerarFuncion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button BNumPares;
+        private System.Windows.Forms.Button BNumPrimos;
+        private System.Windows.Forms.Button BNumImpares;
+        private System.Windows.Forms.ListBox TBListaNumeros;
     }
 }
 
--- TrabajosPracticos45/Form1.cs
+++ TrabajosPracticos45/Form1.cs
@@ -16,5 +16,60 @@ namespace TrabajosPracticos45
         {
             InitializeComponent();
         }
+
+        private void BGenerarFuncion_Click(object sender, EventArgs e)
+        {
+            // Variables para almacenar los valores desde y hasta
+            int desde, hasta;
+
+            // Validación: Verifica que los TextBox no estén vacíos y que contengan solo números
+            if (string.IsNullOrWhiteSpace(TDesde.Text) || !int.TryParse(TDesde.Text, out desde))
+            {
+                MessageBox.Show("Ingrese un valor numérico válido en 'Desde'", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(THasta.Text) || !int.TryParse(THasta.Text, out hasta))
+            {
+                MessageBox.Show("Ingrese un valor numérico válido en 'Hasta'", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Limpiar el ListBox antes de agregar los números
+            TBListaNumeros.Items.Clear();
+
+            // Validación: Asegúrate de que 'desde' sea menor o igual a 'hasta'
+            if (desde <= hasta)
+            {
+                // Generar los números y agregarlos al ListBox
+                for (int i = desde; i <= hasta; i++)
+                {
+                    TBListaNumeros.Items.Add(i);
+                }
+            } 
+            else 
+            { 
+                MessageBox.Show("'Desde' debe ser menor o igual a 'Hasta'", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void TDesde_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Verifica si la tecla presionada no es un número y no es la tecla de retroceso
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true; // Cancela la entrada de la tecla
+            }
+        }
+
+        private void THasta_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Verifica si la tecla presionada no es un número y no es la tecla de retroceso
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true; // Cancela la entrada de la tecla
+            }
+        }
     }
 }
