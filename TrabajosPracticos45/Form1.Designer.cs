c174199e867a660c8a25908a5ee89e6464de70e3
malen <malenpinto9@gmail.com>
2024-08-23 15:10:48 -0300

    actualizacion

--- TrabajosPracticos45/Form1.Designer.cs
+++ TrabajosPracticos45/Form1.Designer.cs
@@ -44,9 +44,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(53, 101);
+            this.label1.Location = new System.Drawing.Point(40, 82);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.Size = new System.Drawing.Size(49, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Desde";
             // 
@@ -54,34 +55,38 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(53, 154);
+            this.label2.Location = new System.Drawing.Point(40, 125);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.Size = new System.Drawing.Size(45, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "Hasta";
             // 
             // TDesde
             // 
-            this.TDesde.Location = new System.Drawing.Point(133, 101);
+            this.TDesde.Location = new System.Drawing.Point(100, 82);
+            this.TDesde.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.TDesde.Name = "TDesde";
-            this.TDesde.Size = new System.Drawing.Size(94, 22);
+            this.TDesde.Size = new System.Drawing.Size(72, 20);
             this.TDesde.TabIndex = 2;
             this.TDesde.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TDesde_KeyPress);
             // 
             // THasta
             // 
-            this.THasta.Location = new System.Drawing.Point(133, 154);
+            this.THasta.Location = new System.Drawing.Point(100, 125);
+            this.THasta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.THasta.Name = "THasta";
-            this.THasta.Size = new System.Drawing.Size(94, 22);
+            this.THasta.Size = new System.Drawing.Size(72, 20);
             this.THasta.TabIndex = 3;
             this.THasta.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.THasta_KeyPress);
             // 
             // BGenerarFuncion
             // 
             this.BGenerarFuncion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BGenerarFuncion.Location = new System.Drawing.Point(35, 222);
+            this.BGenerarFuncion.Location = new System.Drawing.Point(26, 180);
+            this.BGenerarFuncion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BGenerarFuncion.Name = "BGenerarFuncion";
-            this.BGenerarFuncion.Size = new System.Drawing.Size(208, 28);
+            this.BGenerarFuncion.Size = new System.Drawing.Size(156, 23);
             this.BGenerarFuncion.TabIndex = 4;
             this.BGenerarFuncion.Text = "Generar Función";
             this.BGenerarFuncion.UseVisualStyleBackColor = true;
@@ -91,28 +96,32 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(377, 65);
+            this.label3.Location = new System.Drawing.Point(283, 53);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(142, 20);
+            this.label3.Size = new System.Drawing.Size(119, 17);
             this.label3.TabIndex = 5;
             this.label3.Text = "Lista de Números";
             // 
             // BNumPares
             // 
             this.BNumPares.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BNumPares.Location = new System.Drawing.Point(35, 270);
+            this.BNumPares.Location = new System.Drawing.Point(26, 219);
+            this.BNumPares.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BNumPares.Name = "BNumPares";
-            this.BNumPares.Size = new System.Drawing.Size(208, 28);
+            this.BNumPares.Size = new System.Drawing.Size(156, 23);
             this.BNumPares.TabIndex = 7;
             this.BNumPares.Text = "Números Pares";
             this.BNumPares.UseVisualStyleBackColor = true;
+            this.BNumPares.Click += new System.EventHandler(this.BNumPares_Click);
             // 
             // BNumPrimos
             // 
             this.BNumPrimos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BNumPrimos.Location = new System.Drawing.Point(35, 367);
+            this.BNumPrimos.Location = new System.Drawing.Point(26, 298);
+            this.BNumPrimos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BNumPrimos.Name = "BNumPrimos";
-            this.BNumPrimos.Size = new System.Drawing.Size(208, 28);
+            this.BNumPrimos.Size = new System.Drawing.Size(156, 23);
             this.BNumPrimos.TabIndex = 8;
             this.BNumPrimos.Text = "Números Primos";
             this.BNumPrimos.UseVisualStyleBackColor = true;
@@ -120,9 +129,10 @@
             // BNumImpares
             // 
             this.BNumImpares.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BNumImpares.Location = new System.Drawing.Point(35, 319);
+            this.BNumImpares.Location = new System.Drawing.Point(26, 259);
+            this.BNumImpares.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BNumImpares.Name = "BNumImpares";
-            this.BNumImpares.Size = new System.Drawing.Size(208, 28);
+            this.BNumImpares.Size = new System.Drawing.Size(156, 23);
             this.BNumImpares.TabIndex = 9;
             this.BNumImpares.Text = "Números Impares";
             this.BNumImpares.UseVisualStyleBackColor = true;
@@ -130,17 +140,17 @@
             // TBListaNumeros
             // 
             this.TBListaNumeros.FormattingEnabled = true;
-            this.TBListaNumeros.ItemHeight = 16;
-            this.TBListaNumeros.Location = new System.Drawing.Point(281, 103);
+            this.TBListaNumeros.Location = new System.Drawing.Point(211, 84);
+            this.TBListaNumeros.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.TBListaNumeros.Name = "TBListaNumeros";
-            this.TBListaNumeros.Size = new System.Drawing.Size(363, 340);
+            this.TBListaNumeros.Size = new System.Drawing.Size(273, 277);
             this.TBListaNumeros.TabIndex = 10;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(693, 501);
+            this.ClientSize = new System.Drawing.Size(520, 407);
             this.Controls.Add(this.TBListaNumeros);
             this.Controls.Add(this.BNumImpares);
             this.Controls.Add(this.BNumPrimos);
@@ -151,6 +161,7 @@
             this.Controls.Add(this.TDesde);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Formulario4";
--- TrabajosPracticos45/Form1.cs
+++ TrabajosPracticos45/Form1.cs
@@ -71,5 +71,42 @@ namespace TrabajosPracticos45
                 e.Handled = true; // Cancela la entrada de la tecla
             }
         }
+
+        private void BNumPares_Click(object sender, EventArgs e)
+        {
+            // Variables para almacenar los valores desde y hasta
+            int desde, hasta;
+
+            // Validación: Verifica que los TextBox no estén vacíos y que contengan solo números
+            if (string.IsNullOrWhiteSpace(TDesde.Text) || !int.TryParse(TDesde.Text, out desde))
+            {
+                MessageBox.Show("Ingrese un valor numérico válido en 'Desde'", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(THasta.Text) || !int.TryParse(THasta.Text, out hasta))
+            {
+                MessageBox.Show("Ingrese un valor numérico válido en 'Hasta'", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Limpiar el ListBox antes de agregar los números
+            TBListaNumeros.Items.Clear();
+
+            // Validación: Asegúrate de que 'desde' sea menor o igual a 'hasta'
+            if (desde <= hasta)
+            {
+                // Generar los números y agregarlos al ListBox
+                for (int i = desde; i <= hasta; i++)
+                {
+                    TBListaNumeros.Items.Add(i);
+                }
+            }
+            else
+            {
+                MessageBox.Show("'Desde' debe ser menor o igual a 'Hasta'", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
     }
 }
