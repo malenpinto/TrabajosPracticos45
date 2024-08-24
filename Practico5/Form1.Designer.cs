fec0c0f5df94580123d7c842065786c1a61a565f
malenpinto <malenpinto9@gmail.com>
2024-08-23 23:31:12 -0300

    Interfaz sin DataGrid

--- Practico5/Form1.Designer.cs
+++ Practico5/Form1.Designer.cs
@@ -28,13 +28,244 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.TFoto = new System.Windows.Forms.TextBox();
+            this.RBMujer = new System.Windows.Forms.RadioButton();
+            this.RBHombre = new System.Windows.Forms.RadioButton();
+            this.TSaldo = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.TApellido = new System.Windows.Forms.TextBox();
+            this.TNombre = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.BFoto = new System.Windows.Forms.Button();
+            this.LSaldo = new System.Windows.Forms.Label();
+            this.LSexo = new System.Windows.Forms.Label();
+            this.LFechaNac = new System.Windows.Forms.Label();
+            this.LApellido = new System.Windows.Forms.Label();
+            this.LNombre = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(675, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(228, 232);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.panel1.Controls.Add(this.TFoto);
+            this.panel1.Controls.Add(this.RBMujer);
+            this.panel1.Controls.Add(this.RBHombre);
+            this.panel1.Controls.Add(this.TSaldo);
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.TApellido);
+            this.panel1.Controls.Add(this.TNombre);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.BFoto);
+            this.panel1.Controls.Add(this.LSaldo);
+            this.panel1.Controls.Add(this.LSexo);
+            this.panel1.Controls.Add(this.LFechaNac);
+            this.panel1.Controls.Add(this.LApellido);
+            this.panel1.Controls.Add(this.LNombre);
+            this.panel1.Location = new System.Drawing.Point(82, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(539, 421);
+            this.panel1.TabIndex = 1;
+            // 
+            // TFoto
+            // 
+            this.TFoto.Location = new System.Drawing.Point(161, 292);
+            this.TFoto.Name = "TFoto";
+            this.TFoto.Size = new System.Drawing.Size(320, 22);
+            this.TFoto.TabIndex = 13;
+            // 
+            // RBMujer
+            // 
+            this.RBMujer.AutoSize = true;
+            this.RBMujer.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RBMujer.ForeColor = System.Drawing.Color.White;
+            this.RBMujer.Location = new System.Drawing.Point(274, 188);
+            this.RBMujer.Name = "RBMujer";
+            this.RBMujer.Size = new System.Drawing.Size(88, 35);
+            this.RBMujer.TabIndex = 12;
+            this.RBMujer.Text = "Mujer";
+            this.RBMujer.UseVisualStyleBackColor = true;
+            // 
+            // RBHombre
+            // 
+            this.RBHombre.AutoSize = true;
+            this.RBHombre.Checked = true;
+            this.RBHombre.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RBHombre.ForeColor = System.Drawing.Color.White;
+            this.RBHombre.Location = new System.Drawing.Point(161, 188);
+            this.RBHombre.Name = "RBHombre";
+            this.RBHombre.Size = new System.Drawing.Size(106, 35);
+            this.RBHombre.TabIndex = 11;
+            this.RBHombre.TabStop = true;
+            this.RBHombre.Text = "Hombre";
+            this.RBHombre.UseVisualStyleBackColor = true;
+            // 
+            // TSaldo
+            // 
+            this.TSaldo.Location = new System.Drawing.Point(161, 239);
+            this.TSaldo.Name = "TSaldo";
+            this.TSaldo.Size = new System.Drawing.Size(320, 22);
+            this.TSaldo.TabIndex = 10;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(274, 140);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(125, 22);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // TApellido
+            // 
+            this.TApellido.Location = new System.Drawing.Point(161, 86);
+            this.TApellido.Name = "TApellido";
+            this.TApellido.Size = new System.Drawing.Size(320, 22);
+            this.TApellido.TabIndex = 8;
+            // 
+            // TNombre
+            // 
+            this.TNombre.Location = new System.Drawing.Point(161, 31);
+            this.TNombre.Name = "TNombre";
+            this.TNombre.Size = new System.Drawing.Size(320, 22);
+            this.TNombre.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(193, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 76);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Agregar";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // BFoto
+            // 
+            this.BFoto.Location = new System.Drawing.Point(29, 286);
+            this.BFoto.Name = "BFoto";
+            this.BFoto.Size = new System.Drawing.Size(104, 35);
+            this.BFoto.TabIndex = 5;
+            this.BFoto.Text = "Foto";
+            this.BFoto.UseVisualStyleBackColor = true;
+            this.BFoto.Click += new System.EventHandler(this.BFoto_Click);
+            // 
+            // LSaldo
+            // 
+            this.LSaldo.AutoSize = true;
+            this.LSaldo.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LSaldo.ForeColor = System.Drawing.Color.White;
+            this.LSaldo.Location = new System.Drawing.Point(25, 227);
+            this.LSaldo.Name = "LSaldo";
+            this.LSaldo.Size = new System.Drawing.Size(108, 44);
+            this.LSaldo.TabIndex = 4;
+            this.LSaldo.Text = "Saldo: ";
+            // 
+            // LSexo
+            // 
+            this.LSexo.AutoSize = true;
+            this.LSexo.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LSexo.ForeColor = System.Drawing.Color.White;
+            this.LSexo.Location = new System.Drawing.Point(25, 183);
+            this.LSexo.Name = "LSexo";
+            this.LSexo.Size = new System.Drawing.Size(93, 44);
+            this.LSexo.TabIndex = 3;
+            this.LSexo.Text = "Sexo:";
+            // 
+            // LFechaNac
+            // 
+            this.LFechaNac.AutoSize = true;
+            this.LFechaNac.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LFechaNac.ForeColor = System.Drawing.Color.White;
+            this.LFechaNac.Location = new System.Drawing.Point(25, 130);
+            this.LFechaNac.Name = "LFechaNac";
+            this.LFechaNac.Size = new System.Drawing.Size(249, 44);
+            this.LFechaNac.TabIndex = 2;
+            this.LFechaNac.Text = "Fecha Nacimiento: ";
+            // 
+            // LApellido
+            // 
+            this.LApellido.AutoSize = true;
+            this.LApellido.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LApellido.ForeColor = System.Drawing.Color.White;
+            this.LApellido.Location = new System.Drawing.Point(25, 74);
+            this.LApellido.Name = "LApellido";
+            this.LApellido.Size = new System.Drawing.Size(129, 44);
+            this.LApellido.TabIndex = 1;
+            this.LApellido.Text = "Apellido:";
+            // 
+            // LNombre
+            // 
+            this.LNombre.AutoSize = true;
+            this.LNombre.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LNombre.ForeColor = System.Drawing.Color.White;
+            this.LNombre.Location = new System.Drawing.Point(25, 19);
+            this.LNombre.Name = "LNombre";
+            this.LNombre.Size = new System.Drawing.Size(130, 44);
+            this.LNombre.TabIndex = 0;
+            this.LNombre.Text = "Nombre: ";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(987, 673);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Formulario con Grid";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label LSaldo;
+        private System.Windows.Forms.Label LSexo;
+        private System.Windows.Forms.Label LFechaNac;
+        private System.Windows.Forms.Label LApellido;
+        private System.Windows.Forms.Label LNombre;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BFoto;
+        private System.Windows.Forms.TextBox TApellido;
+        private System.Windows.Forms.TextBox TNombre;
+        private System.Windows.Forms.TextBox TSaldo;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.RadioButton RBHombre;
+        private System.Windows.Forms.RadioButton RBMujer;
+        private System.Windows.Forms.TextBox TFoto;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- Practico5/Form1.cs
+++ Practico5/Form1.cs
@@ -16,5 +16,25 @@ namespace Practico5
         {
             InitializeComponent();
         }
+
+        private void BFoto_Click(object sender, EventArgs e)
+        {
+            // Configurar el OpenFileDialog para seleccionar imágenes
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Archivos de imagen|*.jpg;*.jpeg;*.png;*.bmp";
+
+            // Mostrar el cuadro de diálogo y verificar si el usuario selecciona un archivo
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Obtener la ruta completa del archivo seleccionado
+                string rutaImagen = openFileDialog.FileName;
+
+                // Mostrar la imagen en el PictureBox
+                pictureBox1.Image = Image.FromFile(rutaImagen);
+
+                // Mostrar la ruta de la imagen en el TextBox TxtFoto
+                TFoto.Text = rutaImagen;
+            }
+        }
     }
 }
