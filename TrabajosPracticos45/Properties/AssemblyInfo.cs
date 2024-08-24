27e6189c657896af610ac5112eec173e7b1d9dce
malenpinto <malenpinto9@gmail.com>
2024-08-24 00:41:46 -0300

    A

--- TrabajosPracticos45/Properties/AssemblyInfo.cs
+++ TrabajosPracticos45/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("TrabajosPracticos45")]
+[assembly: AssemblyTitle("Practico4")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
